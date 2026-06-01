5ac95f778d3eb07ce60e6d979af6d259d65b5727
Edi <ediyanghk@gmail.com>
2026-06-01 11:26:38 +0800

    Fix: disable @next/next/no-img-element for demo template img usage

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -37,6 +37,8 @@ const commonRules = () => ({
   'react/react-in-jsx-scope': 0,
   'react/jsx-no-useless-fragment': [1, { allowExpressions: true }],
   'react/jsx-curly-brace-presence': [2, { props: 'never', children: 'never' }],
+  // next
+  '@next/next/no-img-element': 0,
   // typescript
   '@typescript-eslint/no-shadow': 2,
   '@typescript-eslint/no-explicit-any': 0,
